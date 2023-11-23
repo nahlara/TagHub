bbe3a0ab755ab9b3b6cca1cb626ade8788102350
nahlara <150865226+nahlara@users.noreply.github.com>
2023-11-23 23:20:29 +0000

    Numeros

---
+++ provaValdecir/LoopWhile.java
@@ -0,0 +1,9 @@
+public class LoopWhile {
+    public static void main(String[] args) {
+        int aux = 0;
+        while(aux <= 50) {
+            System.out.println("numero:" + aux);
+            aux++;
+        }
+    }
+}
